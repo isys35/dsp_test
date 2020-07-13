2e2b3dfb51972102468e93389c6acc102bc82d89
isys35 <isys35@mail.ru>
2020-07-13 15:23:07 +0300

    check photo

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
 import telebot
 import config
 import db
-
+import requests
+import cv2
 
 bot = telebot.TeleBot(config.TOKEN)
 
@@ -13,7 +14,6 @@ def welcome(message):
 
 @bot.message_handler(content_types=['audio'])
 def save_audio(message):
-    # bot.send_audio(message.chat.id, message.audio.file_id)
     audio_url = "https://api.telegram.org/file/bot{}/{}".format(config.TOKEN,
                                                                 bot.get_file(message.audio.file_id).file_path)
     user_id = message.from_user.id
@@ -21,4 +21,30 @@ def save_audio(message):
     bot.send_audio(message.chat.id, wav_file)
 
 
-bot.polling(timeout=1)
+@bot.message_handler(content_types=['photo'])
+def save_photo(message):
+    photo_url = "https://api.telegram.org/file/bot{}/{}".format(config.TOKEN,
+                                                                bot.get_file(message.photo[-1].file_id).file_path)
+    check_photo(photo_url)
+
+
+def check_photo(photo_url):
+    file_name = photo_url.split('/')[-1]
+    resp = requests.get(photo_url)
+    image_path = f"photo/{file_name}"
+    with open(image_path, 'wb') as photo_file:
+        photo_file.write(resp.content)
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    image = cv2.imread(image_path)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(10, 10)
+    )
+    print(faces)
+
+
+if __name__ == '__main__':
+    bot.polling(timeout=1)
