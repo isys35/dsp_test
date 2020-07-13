5c4601348a15592c9dae4c6c05d0f27351eb55a0
isys35 <isys35@mail.ru>
2020-07-13 14:22:51 +0300

    init

---
+++ bot.py
@@ -0,0 +1,23 @@
+import telebot
+import config
+import db
+
+
+bot = telebot.TeleBot(config.TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    bot.send_message(message.chat.id, 'Тестовый бот для DSP Labs')
+
+
+@bot.message_handler(content_types=['audio'])
+def save_audio(message):
+    # bot.send_audio(message.chat.id, message.audio.file_id)
+    audio_url = "https://api.telegram.org/file/bot{}/{}".format(config.TOKEN,
+                                                                bot.get_file(message.audio.file_id).file_path)
+    user_id = message.from_user.id
+    db.add_audio(audio_url, user_id)
+
+
+bot.polling(timeout=1)
---
+++ db.py
@@ -0,0 +1,43 @@
+import sqlite3
+import config
+import os
+import requests
+from pydub import AudioSegment
+
+def create_db():
+    file = open(config.DB_NAME, 'wb')
+    file.close()
+    conn = sqlite3.connect(config.DB_NAME)
+    cursor = conn.cursor()
+    cursor.execute("""CREATE TABLE audio 
+            (id INTEGER PRIMARY KEY AUTOINCREMENT, 
+            path TEXT,
+            user_id INTEGER)""")
+    cursor.execute("""CREATE TABLE photo 
+            (id INTEGER PRIMARY KEY AUTOINCREMENT, 
+            path TEXT,
+            user_id INTEGER)""")
+    if not os.path.exists('audio'):
+        os.mkdir('audio')
+    if not os.path.exists('photo'):
+        os.mkdir('photo')
+    cursor.close()
+    conn.close()
+
+
+def add_audio(audio_url, user_id):
+    file_name = audio_url.split('/')[-1]
+    resp = requests.get(audio_url)
+    with open(f"audio/{file_name}", 'wb') as audio_file:
+        audio_file.write(resp.content)
+    conn = sqlite3.connect(config.DB_NAME)
+    cursor = conn.cursor()
+    query = """INSERT INTO audio (path, user_id) VALUES (?, ?)"""
+    cursor.execute(query, [f"audio/{file_name}", int(user_id)])
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+if __name__ == '__main__':
+    create_db()
